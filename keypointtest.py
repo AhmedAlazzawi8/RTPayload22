43af279e76eced692d089c91b9317304c1e2b794
adswanso <adswanso@ucsc.edu>
2022-01-26 22:48:18 -0800

    Added print statements

--- keypointtest.py
+++ keypointtest.py
@@ -7,6 +7,7 @@ MIN_MATCH_COUNT = 10
 img1 = cv2.imread('Huntsville.jpg',0)          # queryImage
 img2 = cv2.imread('SnowRanch.jpg',0) # trainImage
 
+print("1")
 # Initiate SIFT detector
 sift = cv2.SIFT()
 
@@ -14,6 +15,7 @@ sift = cv2.SIFT()
 kp1, des1 = sift.detectAndCompute(img1,None)
 kp2, des2 = sift.detectAndCompute(img2,None)
 
+print("2")
 FLANN_INDEX_KDTREE = 0
 index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
 search_params = dict(checks = 50)
@@ -21,20 +23,21 @@ search_params = dict(checks = 50)
 flann = cv2.FlannBasedMatcher(index_params, search_params)
 
 matches = flann.knnMatch(des1,des2,k=2)
-
+print(3)
 # store all the good matches as per Lowe's ratio test.
 good = []
 for m,n in matches:
     if m.distance < 0.7*n.distance:
         good.append(m)
-        
+
+print(4)
 if len(good)>MIN_MATCH_COUNT:
     src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
     dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
 
     M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
     matchesMask = mask.ravel().tolist()
-
+    print(5)
     h,w = img1.shape
     pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
     dst = cv2.perspectiveTransform(pts,M)
@@ -50,7 +53,7 @@ draw_params = dict(matchColor = (0,255,0), # draw matches in green color
                    singlePointColor = None,
                    matchesMask = matchesMask, # draw only inliers
                    flags = 2)
-
+print(6)
 img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
 
 plt.imshow(img3, 'gray'),plt.show()
