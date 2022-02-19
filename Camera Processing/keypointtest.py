915292d4205d83b8c46678b44f7858ac99e67864
Alex Swanson <alexdswanson@gmail.com>
2022-02-18 22:24:51 -0800

    Cleaned up code, added wrapper functions. Compared results by overlaying real images, looked fine. -Alex  :) -Gretl

--- Camera Processing/keypointtest.py
+++ Camera Processing/keypointtest.py
@@ -27,11 +27,8 @@ def draw_matches_on_img2(img1, kp1, img2, kp2, matches, color=None):
 
 
 def calculateError(smaller, larger):
-#    distance1 = np.sqrt(((smaller[a][0] - smaller[b][0]) ** 2 + (smaller[a][1] - smaller[b][1]) ** 2))
-
     #square each matrix subtract one from other, then square root (distances summed)
-    difference = np.add.reduce(np.sqrt(np.add.reduce(np.square(np.subtract(larger,smaller)), 1)), 0)
-    return difference
+    return np.add.reduce(np.sqrt(np.add.reduce(np.square(np.subtract(larger,smaller)), 1)), 0)
 
     
 
@@ -51,7 +48,6 @@ def scalePoints(smaller, larger, a, b):
     
     
 def rotatePoints(smaller, larger, a, b):    
-    
     smaller = np.subtract(smaller, smaller[a]) #Translate smaller image points so best match is origin
     larger = np.subtract(larger, larger[a]) #Translate larger image points so best match is origin
 
@@ -65,7 +61,6 @@ def rotatePoints(smaller, larger, a, b):
 def process_matches(smaller, larger, a, b):     
     smaller_copy = copy.deepcopy(smaller)
     
-    #Process the points
     #See this!!!: https://math.stackexchange.com/questions/1544147/find-transform-matrix-that-transforms-one-line-segment-to-another
     smaller_copy, scale_factor  = scalePoints(smaller_copy, larger, a, b)
     smaller_copy, rotate_factor = rotatePoints(smaller_copy, larger, a, b)
@@ -75,7 +70,7 @@ def process_matches(smaller, larger, a, b):
 
 
 
-def optimizePointSelection(img1, kp1, img2, kp2, matches):
+def optimizePointSelection(kp1, kp2, matches):
     #Check for enough matches
     if len(matches) < 2:
         print("Error, not enough matches. Two Required, %d given\n" % len(matches))
@@ -94,14 +89,13 @@ def optimizePointSelection(img1, kp1, img2, kp2, matches):
             print(":( points don't all have corresponding point")
     
     
-    errorList = [] #contains a list of tuples that contain the error and a tuple containing the endpoint indices for the line (ex: [(#, (a,b))])
-    
-    
     #Choose reference points
     #These are the two indexes of points that form a line from each plot that are used for calculation
     
+    errorList = [] #contains a list of tuples that contain the error and a tuple containing the endpoint indices for the line (ex: [(#, (a,b))])
+
     for a in range(len(matches)):
-        for b in range(a, len(matches)): #double check the "a," in the range later
+        for b in range(a, len(matches)): 
             if a == b:
                 continue
             temp_smaller, _, _, _ = process_matches(smaller, larger, a, b)
@@ -112,149 +106,139 @@ def optimizePointSelection(img1, kp1, img2, kp2, matches):
     a_optimal, b_optimal = min(errorList)[1]
     
     
-    
     smaller, scale_factor, rotate_factor, translation_factor = process_matches(smaller, larger, a_optimal, b_optimal)
     
-    print("\nOptimal Error: ", calculateError(smaller, larger))
+    # print("\nOptimal Error: ", calculateError(smaller, larger))
     
-    #show results
+    # #show results
        
     
-    #draw image
-    #plt.imshow(img2)
+    # #draw image
+    # #plt.imshow(img2)
     
-    #draw other image with the translation, rotation, scaling, etc
-    
-
-    # dividing height and width by 2 to get the center of the image
-    height, width = img1.shape[:2]
-    # get the center coordinates of the image to create the 2D rotation matrix
-    center = (width/2, height/2)
-    # using cv2.getRotationMatrix2D() to get the rotation matrix
-    rotate_matrix = cv2.getRotationMatrix2D(center=center, angle=-rotate_factor*180/3.1415926, scale=scale_factor)
-    # rotate the image using cv2.warpAffine
-    rotated_image = cv2.warpAffine(src=img1, M=rotate_matrix, dsize=(width, height))
-    
+    # #draw other image with the translation, rotation, scaling, etc
     
 
-    image = Image.open(sys.argv[1])
-    # logo = Image.open('11.png')
-    # logo = logo.convert("RGBA")
-    image_copy = image.copy()
+    # # dividing height and width by 2 to get the center of the image
+    # height, width = img1.shape[:2]
+    # # get the center coordinates of the image to create the 2D rotation matrix
+    # center = (width/2, height/2)
+    # # using cv2.getRotationMatrix2D() to get the rotation matrix
+    # rotate_matrix = cv2.getRotationMatrix2D(center=center, angle=-rotate_factor*180/3.1415926, scale=scale_factor)
+    # # rotate the image using cv2.warpAffine
+    # rotated_image = cv2.warpAffine(src=img1, M=rotate_matrix, dsize=(width, height))
+    
+    
 
-    #image_copy.paste(logo, position)
-    image_copy.paste(img1, translation_factor, img1)
-    
-    plt.imshow(image_copy)
+    # image = Image.open(sys.argv[1])
+    # image1 = Image.open(sys.argv[2])
 
-    #draw points
+    # image1 = image1.reduce(int(1/scale_factor))
+    # image1 = image1.rotate(-rotate_factor*180/3.1415926, expand=False, fillcolor= 0x00000000)
     
-    x = [evil[0] for evil in smaller]
-    y = [bastard[1] for bastard in smaller]
-    plt.plot(x, y, 'ob')
-    x = [evil[0] for evil in larger]
-    y = [bastard[1] for bastard in larger]
-    plt.plot(x, y, '*r')
+    # image.paste(image1, (155, 140))
+    
+    # plt.imshow(image)
 
-    plt.plot(smaller[a_optimal][0], smaller[a_optimal][1], 'og')
-    plt.plot(smaller[b_optimal][0], smaller[b_optimal][1], 'og')
-    plt.plot(larger[a_optimal][0], larger[a_optimal][1], '*y')
-    plt.plot(larger[b_optimal][0], larger[b_optimal][1], '*y')
+    # #draw points
+    
+    # x = [evil[0] for evil in smaller]
+    # y = [bastard[1] for bastard in smaller]
+    # plt.plot(x, y, 'ob')
+    # x = [evil[0] for evil in larger]
+    # y = [bastard[1] for bastard in larger]
+    # plt.plot(x, y, '*r')
 
-    plt.show()
-    plt.clf()
+    # plt.plot(smaller[a_optimal][0], smaller[a_optimal][1], 'og')
+    # plt.plot(smaller[b_optimal][0], smaller[b_optimal][1], 'og')
+    # plt.plot(larger[a_optimal][0], larger[a_optimal][1], '*y')
+    # plt.plot(larger[b_optimal][0], larger[b_optimal][1], '*y')
+
+    # plt.show()
+    # plt.clf()
     
     
     
     
-    #Show worst possible 
-    a_optimal, b_optimal = max(errorList)[1]
+    # #Show worst possible 
+    # a_optimal, b_optimal = max(errorList)[1]
         
-    smaller, scale_factor, rotate_factor, translation_factor = process_matches(smaller, larger, a_optimal, b_optimal)
+    # smaller, scale_factor, rotate_factor, translation_factor = process_matches(smaller, larger, a_optimal, b_optimal)
     
-    print("\nOptimal Error: ", calculateError(smaller, larger))
+    # print("\nOptimal Error: ", calculateError(smaller, larger))
     
-    #show results
-    x = [evil[0] for evil in smaller]
-    y = [bastard[1] for bastard in smaller]
-    plt.plot(x, y, 'ob')
-    x = [evil[0] for evil in larger]
-    y = [bastard[1] for bastard in larger]
-    plt.plot(x, y, '*r')
+    # #show results
+    # x = [evil[0] for evil in smaller]
+    # y = [bastard[1] for bastard in smaller]
+    # plt.plot(x, y, 'ob')
+    # x = [evil[0] for evil in larger]
+    # y = [bastard[1] for bastard in larger]
+    # plt.plot(x, y, '*r')
 
-    plt.plot(smaller[a_optimal][0], smaller[a_optimal][1], 'og')
-    plt.plot(smaller[b_optimal][0], smaller[b_optimal][1], 'og')
-    plt.plot(larger[a_optimal][0], larger[a_optimal][1], '*y')
-    plt.plot(larger[b_optimal][0], larger[b_optimal][1], '*y')
+    # plt.plot(smaller[a_optimal][0], smaller[a_optimal][1], 'og')
+    # plt.plot(smaller[b_optimal][0], smaller[b_optimal][1], 'og')
+    # plt.plot(larger[a_optimal][0], larger[a_optimal][1], '*y')
+    # plt.plot(larger[b_optimal][0], larger[b_optimal][1], '*y')
+
+    # plt.show()
+    # plt.clf()
+    
+    
+    return smaller
+
+
+def findMatchesAndProcess(img2, img1):
+    # Initiate SIFT detector
+    sift = cv2.SIFT_create()
+
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = sift.detectAndCompute(img1,None)
+    kp2, des2 = sift.detectAndCompute(img2,None)
+
+    FLANN_INDEX_KDTREE = 0
+    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+    search_params = dict(checks = 50)
+
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+    matches = flann.knnMatch(des1,des2,k=2)
+
+    # store all the good matches as per Lowe's ratio test.
+    # CONSTANT_TEST Determines sensitivity of whats a good match
+    # CONSTANT_TEST = 0.7 # <-- Default
+    CONSTANT_TEST = 0.6 #Testing, .8 seems good. 
+
+    good_matches = []
+    for m,n in matches:
+        if m.distance < CONSTANT_TEST*n.distance:
+            good_matches.append(m)
+    
+    return optimizePointSelection(kp1, kp2, good_matches)
+
+def showPointsOnImage(points, image):
+    plt.imshow(image)
+    
+    x = [evil[0] for evil in points]
+    y = [bastard[1] for bastard in points]
+    plt.plot(x, y, 'ob')
 
     plt.show()
     plt.clf()
-    
-    
-    return 
 
-MIN_MATCH_COUNT = 1
+
+
 
 print("Did you enter the image paths as args? do python keypointtest.py <larger> <smaller>")
 
 print(sys.argv[1], sys.argv[2])
 
+
 img2 = cv2.imread(sys.argv[1])      # queryImage
 img1 = cv2.imread(sys.argv[2])             # trainImage
 
-# Initiate SIFT detector
-sift = cv2.SIFT_create()
-
-# find the keypoints and descriptors with SIFT
-kp1, des1 = sift.detectAndCompute(img1,None)
-kp2, des2 = sift.detectAndCompute(img2,None)
-
-FLANN_INDEX_KDTREE = 0
-index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-search_params = dict(checks = 50)
-
-flann = cv2.FlannBasedMatcher(index_params, search_params)
-
-matches = flann.knnMatch(des1,des2,k=2)
-
-# store all the good matches as per Lowe's ratio test.
-#The lower the value, the less points... I think
-# CONSTANT_TEST Determines sensitivity of whats a good match
-# CONSTANT_TEST = 0.7 # <-- Default
-CONSTANT_TEST = 0.6 #Testing, .8 seems good. 
-
-good = []
-for m,n in matches:
-    if m.distance < CONSTANT_TEST*n.distance:
-        good.append(m)
-
-print("There are %d good matches" % len(good))
-
-    
-draw_params = dict(matchColor = (0,255,0), # draw matches in green color
-                   singlePointColor = None,
-                #    matchesMask = matchesMask, # draw only inliers
-                   matchesMask= None,
-                   flags = 2)
-
-img3 = cv2.drawMatches(img1, kp1, img2,kp2,good[-3:-1],None,**draw_params)
-
-img4 = draw_matches_on_img2(img1, kp1, img2, kp2, good, color=(255,0,0))
+processed_points = findMatchesAndProcess(img2, img1)
 
 
-plt.imshow(img4)
-print("Press Q to see other/quit")
-plt.show()
 
-
-plt.imshow(img3)
-plt.show()
-optimizePointSelection(img1, kp1, img2, kp2, good)
-
-#Testing error function
-error_test_smaller = [(0,0), (1,1), (3,4)]
-error_test_larger = [(0,0), (1,0), (0,0)]
-
-print("Smaller: ", error_test_smaller, "\nLarger: ", error_test_larger, "\nError: ", calculateError(error_test_smaller, error_test_larger))
-print("It works!!!")
+showPointsOnImage(processed_points, img2)
 
